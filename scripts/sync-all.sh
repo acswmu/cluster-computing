5d39759dc62aba5660064b1e020d4cb154fc0d06
root <root@overlord-0.duckdns.org>
2016-04-19 10:11:16 -0400

    reorganized repo

---
+++ scripts/sync-all.sh
@@ -0,0 +1,4 @@
+rsync -avxP $1 root@zergling-0:$1
+rsync -avxP $1 root@zergling-1:$1
+rsync -avxP $1 root@zergling-2:$1
+rsync -avxP $1 root@overlord-0:$1
---
+++ scripts/sync-nodes.sh
@@ -0,0 +1,3 @@
+rsync -avxP $1 root@zergling-0:$1
+rsync -avxP $1 root@zergling-1:$1
+rsync -avxP $1 root@zergling-2:$1
