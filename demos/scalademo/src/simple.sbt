009e5086038f1a7da0ae35f0b16304a1020a6a5e
root <root@overlord-0.duckdns.org>
2016-04-20 00:01:49 -0400

    graphx example

--- demos/scalademo/src/simple.sbt
+++
@@ -1,7 +0,0 @@
-name := "Simple Project"
-
-version := "1.0"
-
-scalaVersion := "2.10.5"
-
-libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.1"
--- scripts/scala-deploy.sh
+++ scripts/scala-deploy.sh
@@ -2,4 +2,4 @@ echo "Requires class and project folder"
 spark-submit \
 --class $1 \
 --master spark://192.168.1.1:7077 \
-/root/cluster-computing/demos/$2/src/target/scala-2.10/simple-project_2.10-1.0.jar
+/root/cluster-computing/demos/$2/target/scala-2.10/*.jar
