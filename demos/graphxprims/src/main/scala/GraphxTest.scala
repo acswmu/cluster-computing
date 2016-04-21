1d1c21ed9dc163cb2d7f628ee633b2c409e2392e
root <root@overlord-0.duckdns.org>
2016-04-21 09:55:47 -0400

    graphx prims

---
+++ demos/graphxprims/src/main/scala/GraphxTest.scala
@@ -0,0 +1,50 @@
+/* graphxtest.scala */
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.graphx._
+import org.apache.spark.rdd.RDD
+
+object GraphxTest {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Graphx Test")
+    val sc = new SparkContext(conf)
+val vertexArray = Array(
+  (1L, ("Alice", 28)),
+  (2L, ("Bob", 27)),
+  (3L, ("Charlie", 65)),
+  (4L, ("David", 42)),
+  (5L, ("Ed", 55)),
+  (6L, ("Fran", 50))
+  )
+val edgeArray = Array(
+  Edge(2L, 1L, 7),
+  Edge(2L, 4L, 2),
+  Edge(3L, 2L, 4),
+  Edge(3L, 6L, 3),
+  Edge(4L, 1L, 1),
+  Edge(5L, 2L, 2),
+  Edge(5L, 3L, 8),
+  Edge(5L, 6L, 3)
+  )
+
+val vertexRDD: RDD[(Long, (String, Int))] = sc.parallelize(vertexArray)
+val edgeRDD: RDD[Edge[Int]] = sc.parallelize(edgeArray)
+
+val graph: Graph[(String, Int), Int] = Graph(vertexRDD, edgeRDD)
+
+graph.vertices.filter {
+  case (id, (name, age)) => age > 30
+}.collect.foreach {
+  case (id, (name, age)) => println(s"$name is $age")
+}
+
+//    val logFile = "/usr/local/spark/README.md" // Should be some file on your system
+//    val conf = new SparkConf().setAppName("Graphx Test")
+//    val sc = new SparkContext(conf)
+//    val logData = sc.textFile(logFile, 2).cache()
+//    val numAs = logData.filter(line => line.contains("a")).count()
+//    val numBs = logData.filter(line => line.contains("b")).count()
+//    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+  }
+}
---
+++ demos/pprimsalgo/src/main/scala/PrimsAlgo.scala
@@ -0,0 +1,121 @@
+/*
+ * Prims Algorthm
+ * CS4310
+ */
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import scala.io.Source
+
+class Node(id: Int) {
+  var edges = List[Edge]()
+
+  def addEdge(other: Node, weight: Int) = {
+    val edge = new Edge(this, other, weight)
+    edges = edge :: edges
+    edge
+  }
+
+  override def toString() = id.toString
+}
+
+class Edge(val from: Node, val to: Node, val weight: Int) extends Ordered[Edge] {
+  // Inverse ordering; should really be external.
+  def compare(that: Edge) = that.weight compare weight
+  override def toString() = from + "," + to + "," + weight
+}
+
+object PrimsAlgo {
+
+  def main(args: Array[String]) {
+    val logFile = "/root/cluster-computing/demos/primsalgo/RawData.csv" // Should be some file on your system
+    val conf = new SparkConf().setAppName("Prims Algorthm")
+    val sc = new SparkContext(conf)
+    val logData = sc.textFile(logFile, 2).cache()
+
+
+    var count = 0
+    val rowArray = readCSV(logFile,sc)
+    val colArray = rowArray.transpose
+    val nodesList = (colArray(0).toList ++ colArray(1).toList).distinct
+    var nodeStruct = new Array[Node](nodesList.length + 1)
+
+    while (count < nodesList.length + 1) {
+
+      nodeStruct(count) = new Node(count)
+      count = count + 1
+    }
+    count = 0
+
+    while (count < colArray(0).length) {
+      nodeStruct(rowArray(count)(0).toInt).addEdge(nodeStruct(rowArray(count)(1).toInt), rowArray(count)(2).toInt)
+      nodeStruct(rowArray(count)(1).toInt).addEdge(nodeStruct(rowArray(count)(0).toInt), rowArray(count)(2).toInt)
+      count = count + 1
+    }
+    count = 0
+
+    val graph = nodeStruct.toList
+
+    // generateMST(graph).sortWith(_ < _).foreach(println)
+    val readLines = sc.parallelize(generateMST(graph)).collect.toList	
+    var totalLength = 0
+    while (count < readLines.length) {
+      val myLine = readLines(count).toString().split(",")
+      totalLength = totalLength + myLine.last.toInt
+      println(myLine(0) + " <--> " + myLine(1) + "    (" + myLine(2) + ")")
+      count = count + 1
+    }
+    println(totalLength)
+  }
+
+  def readCSV(fileName: String,sc: SparkContext): Array[Array[Double]] = {
+    
+    val logData = sc.textFile(fileName, 6).cache()
+    var matrix: Array[Array[Double]] = Array.empty
+
+    val rrdarr = logData.take(logData.count.toInt)
+    rrdarr.foreach{line =>    
+    val cols = line.split(",").map(_.trim.toDouble)
+    matrix = matrix :+ cols
+    }
+    return matrix
+  }
+
+  // Prim's algorithm:
+  //   * Choose a random node.
+  //   * Place edges from that node in priority queue.
+  //   * While queue not empty:
+  //   *   pop highest weighted edge
+  //   *   if end-point in already-included set, continue
+  //   *   else:
+  //   *     add edge to edge-set
+  //   *     add node to node-set
+  //   *     add edges from new node to queue
+
+  def generateMST(graph: List[Node]): List[Edge] = {
+    import scala.util.Random
+
+    val startNode = graph(Random.nextInt(graph.length))
+
+    var mst_nodes = List(startNode)
+    var mst_edges = List[Edge]()
+
+    val pq = new scala.collection.mutable.PriorityQueue[Edge]
+
+    startNode.edges.foreach(e => pq.enqueue(e))
+
+    while (!pq.isEmpty) {
+      val edge = pq.dequeue
+
+      if (!(mst_nodes contains edge.to)) {
+        mst_nodes = edge.to :: mst_nodes
+        mst_edges = edge :: mst_edges
+
+        edge.to.edges.foreach(e => pq.enqueue(e))
+      }
+    }
+
+    mst_edges
+
+  }
+}
