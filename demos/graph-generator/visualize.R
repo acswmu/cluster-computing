20f03490ab609b108f16dc366279d5b4464a290d
blake <Blakewrege1@gmail.com>
2016-04-27 15:22:04 -0400

    updated visualizer

--- demos/graph-generator/visualize.R
+++ demos/graph-generator/visualize.R
@@ -1,47 +1,53 @@
 library(igraph)
 library(qgraph)
-all_content = readLines("/home/blake/github/cluster-computing/demos/graph-generator/NodeData.txt")
+all_content = readLines("NodeData.txt")
 myheader = all_content[1]
 edge_list = all_content[-1]
 headvars = unlist(strsplit(myheader, ","))
 nodes = as.integer(headvars[1][1])
 edges = as.integer(headvars[2][1])
-mat<-matrix(0, nodes, nodes)
-for (i in 1:edges){
-edgeTemp=as.integer(unlist(strsplit(edge_list[i], ",")))
-mat[edgeTemp[1],edgeTemp[2]] = edgeTemp[3]
-mat[edgeTemp[2],edgeTemp[1]] = edgeTemp[3]
+mat <- matrix(0, nodes, nodes)
+for (i in 1:edges) {
+  edgeTemp = as.integer(unlist(strsplit(edge_list[i], ",")))
+  mat[edgeTemp[1], edgeTemp[2]] = edgeTemp[3]
+  mat[edgeTemp[2], edgeTemp[1]] = edgeTemp[3]
 }
-qgraph(mat,edge.labels=TRUE)
-ig <- graph.adjacency(mat, mode="undirected", weighted=TRUE)
-mstoutput=mst(ig)
+qgraph(mat, edge.labels = TRUE)
+ig <- graph.adjacency(mat, mode = "undirected", weighted = TRUE)
+mstoutput = mst(ig)
 results <- capture.output(mstoutput)
-trimresults = substring(results[4],5)
-s <- strsplit(trimresults, "[^[:digit:]]")
+trimTemp = list()
+resultTemp = list()
+for (i in 4:length(results)) {
+  trimTemp = unlist(strsplit(results[i], "]"))
+  resultTemp = c(resultTemp,trimTemp[2])
+}
+s <- strsplit(unlist(resultTemp), "[^[:digit:]]")
+
 solution <- as.numeric(unlist(s))
 solution <- solution[!is.na(solution)]
-minlist<-matrix(0,length(solution)/2 , 3)
+minlist <- matrix(0, length(solution) / 2 , 3)
 temp1 <- list()
 temp2 <- list()
-for (i in 1:length(solution)){
-if ((i%%2)==1) {
- temp1=c(temp1,solution[i])
-}
-if ((i%%2)==0) {
-  temp2=c(temp2,solution[i])
+for (i in 1:length(solution)) {
+  if ((i %% 2) == 1) {
+    temp1 = c(temp1, solution[i])
   }
-
+  if ((i %% 2) == 0) {
+    temp2 = c(temp2, solution[i])
+  }
+  
 }
-n1=unlist(temp1)
-n2=unlist(temp2)
-for (i in 1:length(solution)/2){
-minlist[i,1]=n1[i]
-minlist[i,2]=n2[i]
-minlist[i,3]=mat[n1[i],n2[i]]
+n1 = unlist(temp1)
+n2 = unlist(temp2)
+for (i in 1:length(solution) / 2) {
+  minlist[i, 1] = n1[i]
+  minlist[i, 2] = n2[i]
+  minlist[i, 3] = mat[n1[i], n2[i]]
 }
 print(minlist)
 print('edge total=')
-print(sum(minlist[,3]))
+print(sum(minlist[, 3]))
 #plot(ig, edge.label=round(E(ig)$weight, 3))
 
 #NodeData.txt
